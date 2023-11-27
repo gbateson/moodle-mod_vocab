90e73d66948842271e14b289d89e28afa588adc4
Gordon Bateson <gordonbateson@gmail.com>
2023-11-27 11:01:04 +0900

    mod_vocab add new subplugin 'ai' to represent AI assistants for Vocab activities

---
+++ classes/plugininfo/vocabai.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * classes/plugininfo/vocabai.php
+ *
+ * @package    mod_vocab
+ * @copyright  2023 Gordon BATESON
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Gordon BATESON https://github.com/gbateson
+ * @since      Moodle 3.11
+ */
+namespace mod_vocab\plugininfo;
+
+use core\plugininfo\base;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * vocabai
+ *
+ * @package    mod_vocab
+ * @copyright  2023 Gordon Bateson
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Gordon Bateson gordonbateson@gmail.com
+ * @since      Moodle 3.11
+ */
+class vocabai extends base {
+
+    /**
+     * is_uninstall_allowed
+     *
+     * @return xxx
+     * @todo Finish documenting this function
+     */
+    public function is_uninstall_allowed() {
+        return true;
+    }
+}
